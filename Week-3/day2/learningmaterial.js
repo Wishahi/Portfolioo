7a7d303a19824f27b280eb5d7451acb20cadf847
Wishahi <33320812+Wishahi@users.noreply.github.com>
2022-09-17 17:32:52 +0300

    Delete Week-3 directory

--- Week-3/day2/learningmaterial.js
+++
@@ -1,8 +0,0 @@
-// function insertRow(){
-//     alert("You clicked me")
-// }
-
-let form = document.forms.form1;
-let firstElement = form.elements.fname;
-let secondElement = form.elements.lname;
-console.log(firstElement.value, secondElement.value);
