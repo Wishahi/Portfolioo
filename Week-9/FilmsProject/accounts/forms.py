204f820206216c967be3fcacc420d3201c274e68
Wishahi <33320812+Wishahi@users.noreply.github.com>
2022-09-17 17:38:21 +0300

    Delete Week-9 directory

--- Week-9/FilmsProject/accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import UserProfile
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        field= '__all__'
-        exclude = ['user']
-
