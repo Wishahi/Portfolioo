b48546d78b13f04f0ecf6ec57792c69a4f6beed1
Wishahi <33320812+Wishahi@users.noreply.github.com>
2022-09-17 17:33:09 +0300

    Delete Week-5 directory

--- Week-5/day3/ExercisesXP/ExercisesXP+Module/exercises_one.py
+++
@@ -1,5 +0,0 @@
-# from func import MyClass
-
-# my_func = MyClass.add
-
-# my_func(5, 6)
--- Week-5/day3/ExercisesXP/ExercisesXP+Module/func.py
+++
@@ -1,7 +0,0 @@
-# class MyClass:
-    
-#   @staticmethod
-#   def add(a, b): 
-#     return a + b
-
-# print(MyClass.add(1,2))
